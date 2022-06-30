d179324e8f80b1c0e68a208bf9784e1c92f10da7
postanogova_elen <postanogovaelena@yandex.ru>
2022-06-30 23:55:00 +0500

    add solution of task4

---
+++ Seminar1/Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿int a, b, c, max;
+
+Console.Write("Введите первое число: ");
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+c = Convert.ToInt32(Console.ReadLine());
+
+max = a;
+if (b > max)  max = b;
+if (c > max)  max = c;
+
+Console.Write(a + ", " + b + ", " + c + " -> max = " + max);
