bd85272ca00943ac6f3f328ea77797e19aecbfc3
postanogova_elen <postanogovaelena@yandex.ru>
2022-07-01 00:03:42 +0500

    add solution of task6

---
+++ Seminar1/Task6/Program.cs
@@ -0,0 +1,17 @@
+﻿int num, talls;
+string message;
+
+Console.Write("Введите число: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+talls = num % 2;
+
+if (talls == 0)
+{
+    message = "четное";
+}
+else
+{
+   message = "не четное";
+}
+Console.WriteLine(num + " -> " + message);
