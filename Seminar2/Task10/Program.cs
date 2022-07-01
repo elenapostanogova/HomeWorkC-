cb0d4e749b50f3cfc6bb8e19021a56212efea9cf
postanogova_elen <postanogovaelena@yandex.ru>
2022-07-01 11:53:18 +0500

    add solution task 10

---
+++ Seminar2/Task10/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+    Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+*/
+
+void FindSecondDigit(int n)
+{
+    int cen = n / 100;
+    int dec = n / 10 - cen * 10;
+    int ed =  n % 10;
+    string messageerror = "Ошибка, Вы ввели не трехзначное число";
+
+
+    if (cen == 0) 
+    {
+        Console.WriteLine(messageerror);
+    }
+    else
+    {
+        if (cen > 10) 
+        {
+            Console.WriteLine(messageerror);
+        }
+        else
+        {
+            Console.WriteLine("Вторая цифра числа " + n + " равна " + dec);
+        }
+    }
+};
+
+Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+FindSecondDigit(num);
